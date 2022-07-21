b2552f142e352748c5555aa1cca7dd1c081968af
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-07-22 08:57:29 +0900

    Cosmetic changes

--- src/main.zig
+++ src/main.zig
@@ -5,33 +5,41 @@ pub fn main() anyerror!void {
     var p = std.json.Parser.init(allocator, false);
     defer p.deinit();
 
-    var args = try std.process.argsWithAllocator(allocator);
-    defer args.deinit();
+    const args = try std.process.argsAlloc(allocator);
+    defer allocator.free(args);
 
     const stdout = std.io.getStdOut().writer();
 
-    _ = args.next();
-
-    var m = std.json.ObjectMap.init(allocator);
-    defer m.deinit();
-
-    var a = std.json.Array.init(allocator);
-    defer a.deinit();
-
     var is_array = false;
-    while (args.next()) |aa| {
+
+    var files = std.ArrayList([]const u8).init(allocator);
+    defer files.deinit();
+
+    for (args[1..args.len]) |aa| {
         if (std.mem.eql(u8, aa, "-a")) {
             is_array = true;
-            continue;
+        } else {
+            try files.append(aa);
         }
+    }
 
-        if (is_array) {
+    if (is_array) {
+        var a = std.json.Array.init(allocator);
+        defer a.deinit();
+
+        for (files.items) |aa| {
             if (p.parse(aa)) |v| {
                 try a.append(v.root);
             } else |_| {
                 try a.append(std.json.Value{ .String = aa });
             }
-        } else {
+        }
+        try (std.json.Value{ .Array = a }).jsonStringify(.{}, stdout);
+    } else {
+        var m = std.json.ObjectMap.init(allocator);
+        defer m.deinit();
+
+        for (files.items) |aa| {
             var it = std.mem.split(u8, aa, "=");
             var key = it.next() orelse "";
             var value = it.next() orelse "";
@@ -41,10 +49,6 @@ pub fn main() anyerror!void {
                 try m.put(key, std.json.Value{ .String = value });
             }
         }
-    }
-    if (is_array) {
-        try (std.json.Value{ .Array = a }).jsonStringify(.{}, stdout);
-    } else {
         try (std.json.Value{ .Object = m }).jsonStringify(.{}, stdout);
     }
     _ = try stdout.write("\n");
